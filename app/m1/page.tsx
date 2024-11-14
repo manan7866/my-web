4080149fd0478348fd124be39ae892f487a4e2d9
Amwighio <159983597+manan7866@users.noreply.github.com>
2024-11-14 07:25:56 -0800

    Update page.tsx

--- app/m1/page.tsx
+++ app/m1/page.tsx
@@ -14,15 +14,31 @@ export default function Home() {
  
   
   return (
-    <div style={{background: "linear-gradient(#007cc4 ,#1c0794)"}} className="h-[100vh] w-full ">
-      {/* <div  className="absolute top-8 right-20 h-[90vh] w-[90%] bg-[rgba(255,255,255,0.7)] rounded-[6px]"></div> */}
-      <div  className="  w-[700px] absolute top-40 left-1/3 rounded-[15px] bg-[rgba(255,255,255,0.7)]">
-        <p style={{textShadow: "2px 2px 0 black"}}  className="text-5xl text-blue-600 text-center">Rusume Builder</p><br/>
+    <div>
+    <div style={{backgroundImage: `url(${bg.src}) `,backgroundSize:"cover"}} className="h-[100vh] flex w-full  items-center justify-center">
+      
+      <div  className="sms:w-72 items-center rounded-[15px] bg-[rgba(255,255,255,0.7)]">
+        <div className="flex">
+      <div className="hidden sms:block mb-2  ">
+        <Image className="" src={own} alt="pic" dir="''"  width={80}/>
+        </div>
+      <p style={{textShadow: "2px 2px 0 black"}}  className="sms:text-2xl w-full sms:content-center text-5xl text-center text-blue-600 " >Rusume Builder</p><br/>
+      </div>
+      
+        
+        
+        
+        
+       <div className="flex"> 
+       
+        <div className="" >
+        
+        
         <ul  >
-          <label style={{textShadow: "1px 1px 0 black"}} className="text-3xl text-blue-600">
+          <label style={{textShadow: "1px 1px 0 black"}} className="sms:text-2xl text-3xl text-blue-600">
           {">"}Personal Information
           </label>
-          <div style={{textShadow: "1px 1px 0 black"}} className=" w-80 text-blue-600 border border-blue-600 rounded-[6px]">
+          <div style={{textShadow: "1px 1px 0 black"}} className="sms:text-xs sms:w-60 w-80 text-blue-600 border border-blue-600 rounded-[6px]">
             <li>
             Name : Abdul manan</li>
             <li>
@@ -33,39 +49,45 @@ export default function Home() {
             </div>
           </ul><br/>
           <ul  >
-          <label style={{textShadow: "1px 1px 0 black"}} className="text-3xl text-blue-600">
+          <label style={{textShadow: "1px 1px 0 black"}} className="sms:text-2xl text-3xl text-blue-600">
           {">"}Education
           </label>
-          <div style={{textShadow: "1px 1px 0 black"}} className="w-80 text-blue-600 border border-blue-600 rounded-[6px]">
+          <div style={{textShadow: "1px 1px 0 black"}} className="sms:text-xs sms:w-60 w-80 text-blue-600 border border-blue-600 rounded-[6px]">
             <li>
             Intermadiate A level</li> 
             </div>
           </ul><br/>
           <ul  >
-          <label style={{textShadow: "1px 1px 0 black"}} className="text-3xl text-blue-600">
+          <label style={{textShadow: "1px 1px 0 black"}} className="sms:text-2xl text-3xl text-blue-600 ">
           {">"}Work Experince
           </label>
-          <div style={{textShadow: "1px 1px 0 black"}} className="w-80 text-blue-600 border border-blue-600 rounded-[6px]">
+          <div style={{textShadow: "1px 1px 0 black"}} className="sms:text-xs sms:w-60 w-80 text-blue-600 border border-blue-600 rounded-[6px]">
             <li>
             Student of Governor Sindh It Course</li> 
             </div>
           </ul><br/>
           {skill && (
           <ul  >
-          <label style={{textShadow: "1px 1px 0 black"}}  className="text-3xl text-blue-600">
+          <label style={{textShadow: "1px 1px 0 black"}}  className="sms:text-2xl text-3xl text-blue-600">
           {">"}Skills
           </label>
-          <div style={{textShadow: "1px 1px 0 black"}} className="w-80 text-blue-600 border border-blue-600 rounded-[6px]">
+          <div style={{textShadow: "1px 1px 0 black"}} className="sms:text-xs sms:w-60 w-80 text-blue-600 border border-blue-600 rounded-[6px]">
             <li>
             Typescript , HTML,CSS,Next JS</li> 
             </div><br/>
           </ul>)}
-        <button className="bg-blue-600 text-white h-20 w-full text-3xl rounded-[6px]" onClick={toggle}>Skills</button>
-        <Image className="absolute top-[60px] right-8" src={own} alt="pic" dir="''"  width={250}/>
         </div>
-       
+        <div className="sms:hidden content-center items-center mx-10">
+        <Image className="" src={own} alt="pic" dir="''"  width={250}/>
+        </div>
+       </div>
+       <div>
+       <button className="bg-blue-600 text-white h-20 w-full text-3xl rounded-[6px]" onClick={toggle}>Skills</button>
+       </div>
+       </div>
       
       
+    </div>
     </div>
     
   );
